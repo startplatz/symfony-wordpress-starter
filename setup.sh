b62f344d33e79bff617af41212fc9897a004022f
Per Bernhardt <per@pixelhouse.de>
2015-01-17 15:50:06 +0100

    Bugfix shell-script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-test -f composer.sh || curl -sS https://getcomposer.org/installer | php
+cd /host
+if [ ! -f composer.phar ]; then
+    curl -sS https://getcomposer.org/installer | php
+fi
 php composer.phar install
+rm -rf /var/www/html
+ln -s /root/web /var/www/html
